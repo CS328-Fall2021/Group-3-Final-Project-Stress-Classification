cf3acc93e1cc7e4f0a20e6f23ed0cc355ff5dd63
mferrara63 <mtferrara@umass.edu>
2021-12-05 16:58:29 -0500

    added all the files we should need

---
+++ activity-classification-test.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+"""
+This is the script used to train an activity recognition 
+classifier on accelerometer data.
+
+"""
+
+import os
+import sys
+import numpy as np
+from sklearn.tree import export_graphviz
+from features import extract_features
+from util import slidingWindow, reorient, reset_vars
+from sklearn import model_selection
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import KFold
+from sklearn.metrics import confusion_matrix
+import pickle
+
+# %%---------------------------------------------------------------------------
+#
+#		                 Load Data From Disk
+#
+# -----------------------------------------------------------------------------
+
+print("Loading data...")
+sys.stdout.flush()
+data_file = 'Test_Data.csv'
+data = np.genfromtxt(data_file, delimiter=',')
+#print(np.isnan(data))
+print("Loaded {} raw labelled activity data samples.".format(len(data)))
+sys.stdout.flush()
+
+# %%---------------------------------------------------------------------------
+#
+#		                    Pre-processing
+#
+# -----------------------------------------------------------------------------
+
+print("Reorienting accelerometer data...")
+sys.stdout.flush()
+reset_vars()
+reoriented = np.asarray([reorient(data[i,1], data[i,2], data[i,3]) for i in range(len(data))])
+reoriented_data_with_timestamps = np.append(data[:,0:1],reoriented,axis=1)
+data = np.append(reoriented_data_with_timestamps, data[:,-1:], axis=1)
+
+# %%---------------------------------------------------------------------------
+#
+#		                Extract Features & Labels
+#
+# -----------------------------------------------------------------------------
+
+window_size = 20
+step_size = 20
+
+# sampling rate should be about 25 Hz; you can take a brief window to confirm this
+n_samples = 1000
+time_elapsed_seconds = (data[n_samples,0] - data[0,0]) / 1000
+sampling_rate = n_samples / time_elapsed_seconds
+
+# TODO: list the class labels that you collected data for in the order of label_index (defined in collect-labelled-data.py)
+class_names = ["walking", "downhill", "sitting", "uphill"] #...
+
+print("Extracting features and labels for window size {} and step size {}...".format(window_size, step_size))
+sys.stdout.flush()
+
+X = []
+Y = []
+
+for i,window_with_timestamp_and_label in slidingWindow(data, window_size, step_size):
+    window = window_with_timestamp_and_label[:,1:-1]   
+    feature_names, x = extract_features(window)
+    X.append(x)
+    Y.append(window_with_timestamp_and_label[10, -1])
+    
+X = np.asarray(X)
+Y = np.asarray(Y)
+n_features = len(X)
+    
+print("Finished feature extraction over {} windows".format(len(X)))
+#print("Unique labels found: {}".format(set(Y)))
+print("\n")
+sys.stdout.flush()
+
+# %%---------------------------------------------------------------------------
+#
+#		                Test & Evaluate Classifier
+#
+# -----------------------------------------------------------------------------
+cv = model_selection.KFold(n_splits=10, random_state=None, shuffle=True)
+
+for train_index, test_index in cv.split(X):
+    #print("TRAIN:", train_index, "TEST:", test_index)
+    X_train, X_test = X[train_index], X[test_index]
+    y_train, y_test = Y[train_index], Y[test_index]
+    
+#pickled_model = pickle.load(open('classifier.pickle', 'rb'))
+#pickled_model.predict(X_test)
+
+with open('classifier.pickle','rb') as modelFile:
+     model = pickle.load(modelFile)
+        
+prediction = model.predict(X)
+#score = model.score(X_test, y_test)
+np.set_printoptions(threshold=np.inf)
+print(prediction)
+#print(score)
---
+++ features.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+"""
+This file is used for extracting features over windows of tri-axial accelerometer 
+data. We recommend using helper functions like _compute_mean_features(window) to 
+extract individual features.
+
+As a side note, the underscore at the beginning of a function is a Python 
+convention indicating that the function has private access (although in reality 
+it is still publicly accessible).
+
+"""
+
+import numpy as np
+import math
+from scipy.stats import entropy
+from scipy.signal import find_peaks
+
+def _compute_mean_features(window):
+    """
+    Computes the mean x, y and z acceleration over the given window. 
+    """
+    
+    return np.mean(window, axis=0)
+
+
+# TODO: define functions to compute more features
+
+def _compute_standard_dev_features(window):
+    return np.std(window, axis=0)
+
+def _compute_dom_freq(window):
+    return np.fft.rfft(window, axis=0).astype(float)[1]
+
+def _compute_max(window):
+    return np.max(window, axis=0)
+
+def _compute_min(window):
+    return np.min(window, axis=0)
+
+# def _compute_peaks(window):
+#     return np.histogram(window, bins=7)[0]
+
+def _compute_peak_len(window):
+    mag = []
+    
+    for e in window:
+        m = np.sqrt((e[0]*e[0])+(e[1]*e[1])+(e[2]*e[2]))
+        mag.append(m)
+    peaks, _ = find_peaks(mag, prominence=1)
+    return [len(peaks)]
+
+
+def extract_features(window):
+    """
+    Here is where you will extract your features from the data over 
+    the given window. We have given you an example of computing 
+    the mean and appending it to the feature vector.
+    
+    """
+    
+    x = []
+    feature_names = []
+
+    x.append(_compute_mean_features(window))
+    feature_names.append("x_mean")
+    feature_names.append("y_mean")
+    feature_names.append("z_mean")
+
+    # TODO: call functions to compute other features. Append the features to x and the names of these features to feature_names
+    x.append(_compute_standard_dev_features(window))
+    feature_names.append("x_std")
+    feature_names.append("y_std")
+    feature_names.append("z_std")
+    
+    x.append(_compute_dom_freq(window))
+    feature_names.append("x_dom")
+    feature_names.append("y_dom")
+    feature_names.append("z_dom")
+    
+    x.append(_compute_max(window))
+    feature_names.append("x_max")
+    feature_names.append("y_max")
+    feature_names.append("z_max")
+
+    x.append(_compute_min(window))
+    feature_names.append("x_min")
+    feature_names.append("y_min")
+    feature_names.append("z_min")
+    
+    x.append(_compute_peak_len(window))
+    feature_names.append("peak length")
+
+
+    feature_vector = np.concatenate(x, axis=0) # convert the list of features to a single 1-dimensional vector
+    return feature_names, feature_vector
